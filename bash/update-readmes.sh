058a23fb33465f4058c767c1e72ae93d90adcdb5
오현재 <55138387+hangooksaram@users.noreply.github.com>
2025-05-07 16:06:24 +0900

    Update update-readmes.sh

--- bash/update-readmes.sh
+++ bash/update-readmes.sh
@@ -7,14 +7,14 @@ while read file; do
     # 첫 번째 # 헤더 추출 (파일 인코딩 UTF-8 가정)
     title=$(grep -m 1 '^# ' "$file" | sed 's/^# //')
     # 타이틀이 없으면 파일명 사용
-    [ -z "$title" ]
+    [ -z "$title" ] && title=$(basename "$file")
     # 링크 생성
-    link="https://github.com/${GITHUB_REPOSITORY}/blob/hosting/$file"
+    link="./$file"
     # 파일이 속한 디렉토리명 추출
     dir=$(dirname "$file")
     dirname_only=$(basename "$dir")
     # hosting 브랜치의 디렉토리와 같은 이름의 디렉토리 내 README 파일 경로
-    readme="$dirname_only/README.md"
+    readme="readme_${dirname_only}.md"
     # README 없으면 생성
     [ -f "$readme" ] || touch "$readme"
     # 이미 해당 타이틀이 기록되어 있지 않으면 추가
